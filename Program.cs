930d802d9e953c9abca3b73a27324e11b0b6da85
Patrick Schlarmann <patrick.schlarmann@sdx-ag.de>
2022-09-21 10:26:35 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var ctx = new BloggingContext();
+
+var someBlogs = await ctx.Blogs
+    .OrderBy(b => b.Id)
+    .Skip(20)
+    .Take(10)
+    .ToArrayAsync();
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs => Set<Blog>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder
+            .UseNpgsql("Host=localhost;Username=postgres;Password=supersecret")
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging();
+}
+
+public class Blog 
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime LastUpdated { get; set; }
+}
